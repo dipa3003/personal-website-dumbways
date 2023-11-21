9ea54e4657fa4f42263c55bc3e39392a1014b652
Dipa Galatian <dipagalatian3003@gmail.com>
2023-11-21 23:14:07 +0700

    rename file html to hbs

--- day11/index.js
+++ day11/index.js
@@ -1,14 +1,26 @@
-import { urlencoded } from "body-parser";
-import express from "express";
-import hbs from "hbs";
+// import express from "express";
+// import path from "path";
+// const path = require("path");
+
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // setup hbs
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
-app.use(urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 app.listen(port, () => {
   console.log(`Personal website | Listening at http://localhost:${port}`);
