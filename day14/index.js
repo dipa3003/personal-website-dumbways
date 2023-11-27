55c4fb779770b0c5d86849d3ee82f83132644c42
Dipa Galatian <dipagalatian3003@gmail.com>
2023-11-27 17:20:04 +0700

    create query select, insert, delete in index and detailProject page

--- day14/index.js
+++ day14/index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const multer = require("multer");
 const config = require("./config/config.json");
 const { Sequelize, QueryTypes } = require("sequelize");
+const { type } = require("os");
+const { Query } = require("pg");
 const sequelize = new Sequelize(config.development);
 
 // setup multer storage
@@ -34,8 +36,8 @@ let dataProjects = [];
 app.get("/", async (req, res) => {
   const query = "SELECT * FROM projects";
   const obj = await sequelize.query(query, { type: QueryTypes.SELECT });
-  console.log("data dr posgres:", obj);
-  console.log("data projects:", dataProjects);
+  // console.log("data dr posgres:", obj);
+  // console.log("data projects:", dataProjects);
   // res.render("index", { dataProjects });
   res.render("index", { dataProjects: obj });
 });
@@ -45,20 +47,24 @@ app.get("/project", (req, res) => {
 });
 
 // Route Detail Project
-app.get("/project/detail/:index", (req, res) => {
-  const { index } = req.params;
-  const dataFilter = dataProjects[parseInt(index)];
+app.get("/project/detail/:id", async (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT * FROM projects WHERE id=${id}`;
+  const obj = await sequelize.query(query, { type: QueryTypes.SELECT });
+  // console.log("data obj: ", obj);
+
+  // const dataFilter = dataProjects[parseInt(index)];
   // console.log(dataFilter);
 
-  res.render("detailProject", { data: dataFilter });
+  res.render("detailProject", { data: obj[0] });
 });
 
 // Route post form add-project
-app.post("/project", upload.single("image"), (req, res) => {
+app.post("/project", upload.single("image"), async (req, res) => {
   // fetch data req.body dari form add project
   const data = req.body;
-  console.log("req.body:", data, "req.file:", req.file);
-  const image = req.file.filename;
+  const image = `${req.file.filename}`;
+  console.log("req.body:", data, "req.file.name:", image);
 
   // validasi durasi project
   let start = Date.parse(data.dateStart);
@@ -70,43 +76,67 @@ app.post("/project", upload.single("image"), (req, res) => {
   let year = Math.floor(month / 12);
 
   const duration = year > 0 ? `${year} tahun` : month > 0 ? `${month} bulan` : `${day} hari`;
+  // console.log("duration:", duration);
 
   // kondisi render logo
-  const html = data.html == "on" ? `<i class="fa-brands fa-html5 fa-2xl"></i>` : "";
-  const css = data.css == "on" ? `<i class="fa-brands fa-css3-alt fa-2xl"></i>` : "";
-  const js = data.js == "on" ? `<i class="fa-brands fa-js fa-2xl"></i>` : "";
-  const react = data.react == "on" ? `<i class="fa-brands fa-react fa-2xl"></i>` : "";
+  // const html = data.html == "on" ? `<i class="fa-brands fa-html5 fa-2xl"></i>` : "";
+  // const css = data.css == "on" ? `<i class="fa-brands fa-css3-alt fa-2xl"></i>` : "";
+  // const js = data.js == "on" ? `<i class="fa-brands fa-js fa-2xl"></i>` : "";
+  // const react = data.react == "on" ? `<i class="fa-brands fa-react fa-2xl"></i>` : "";
+  const tech = [];
 
-  const tech = [html, css, react, js];
+  if (data.html == "on") {
+    tech.push("'html5'");
+  }
+  if (data.css == "on") {
+    tech.push("'css3'");
+  }
+  if (data.js == "on") {
+    tech.push("'js'");
+  }
+  if (data.react == "on") {
+    tech.push("'react'");
+  }
+  // console.log("technologies:", tech);
 
-  let dataSubmit = {
-    title: data.title,
-    description: data.description,
-    duration,
-    tech,
-    image,
-    dateStart: data.dateStart,
-    dateEnd: data.dateEnd,
-  };
-  dataProjects.unshift(dataSubmit);
+  // const html = data.html == "on" ? `html5` : "";
+  // const css = data.css == "on" ? `css3` : "";
+  // const js = data.js == "on" ? `js` : "";
+  // const react = data.react == "on" ? `react` : "";
+
+  const query = `INSERT INTO projects (title,"dateStart","dateEnd",description, technologies, image, duration) VALUES('${data.title}','${data.dateStart}','${data.dateEnd}','${data.description}',ARRAY [${tech}],'${image}','${duration}')`;
+  await sequelize.query(query, { type: QueryTypes.INSERT });
+
+  // let dataSubmit = {
+  //   title: data.title,
+  //   description: data.description,
+  //   duration,
+  //   tech,
+  //   image,
+  //   dateStart: data.dateStart,
+  //   dateEnd: data.dateEnd,
+  // };
+  // dataProjects.unshift(dataSubmit);
 
   res.redirect("/");
 });
 
 // Route UPDATE project
-app.get("/project/edit/:index", (req, res) => {
-  const { index } = req.params;
-  const dataFilter = dataProjects[parseInt(index)];
-
+app.get("/project/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  // const dataFilter = dataProjects[parseInt(index)];
+  const query = `SELECT * FROM projects WHERE id=${id}`;
+  const obj = await sequelize.query(query, { type: QueryTypes.UPDATE });
+  console.log("edit obj:", obj[0]);
   // console.log("dataFilter:", dataFilter);
-  const html = dataFilter.tech.html != "" ? "checked" : "";
-  const css = dataFilter.tech.css != "" ? "checked" : "";
-  const react = dataFilter.tech.react != "" ? "checked" : "";
-  const js = dataFilter.tech.js != "" ? "checked" : "";
+  // const html = obj[0].technologies.html5 ? "checked" : "";
+  // const css = obj[0].technologies.css3 ? "checked" : "";
+  // const react = obj[0].technologies.react ? "checked" : "";
+  // const js = obj[0].technologies.js ? "checked" : "";
 
-  const checkedTech = { html, css, react, js };
+  // const checkedTech = { html, css, react, js };
   // console.log("checkedTech: ", checkedTech);
-  res.render("editProject", { data: dataFilter, checkedTech, index });
+  res.render("editProject", { data: obj[0] });
 });
 
 app.post("/project/edit", upload.single("image"), (req, res) => {
@@ -153,9 +183,12 @@ app.post("/project/edit", upload.single("image"), (req, res) => {
 });
 
 // Route DELETE Project
-app.get("/project/delete/:index", (req, res) => {
-  const { index } = req.params;
-  dataProjects.splice(index, 1);
+app.get("/project/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  // dataProjects.splice(index, 1);
+  const query = `DELETE FROM projects WHERE id=${id}`;
+  await sequelize.query(query, { type: QueryTypes.DELETE });
+
   res.redirect("/");
 });
 
